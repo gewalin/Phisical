020269de1a578d52fbeba22296c4197aa8ff69f6
gewalin <gewwalin3930@gmail.com>
2019-01-08 17:25:54 +0700

    Success Button One

--- app/src/main/java/gewalin/th/ac/rmutl/phisical/DetailvideoActivity.java
+++ app/src/main/java/gewalin/th/ac/rmutl/phisical/DetailvideoActivity.java
@@ -1,13 +1,22 @@
 package gewalin.th.ac.rmutl.phisical;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class DetailvideoActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailvideo);
+
     }
+
+
 }
